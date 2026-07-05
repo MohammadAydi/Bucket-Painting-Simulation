9e91aae21bbae03a2fb1556e2e6372f132f7ae25
Mohamad Alaa Alshahrour <alaaalshahror@gmail.com>
2026-07-05 09:55:18 +0300

    add SpawnParticlesInBucket method to SpawnSystem3D for bucket particle spawning

--- Assets/Scripts/Fluid Sim 3D/Systems/SpawnSystem3D.cs
+++ Assets/Scripts/Fluid Sim 3D/Systems/SpawnSystem3D.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public sealed class SpawnSystem3D
 {
+    readonly ParticleSettings _settings;
     readonly ParticlesSpawner3D _particlesSpawner;
 
     public SpawnSystem3D(ParticleSettings settings)
     {
+        _settings = settings;
         _particlesSpawner = new ParticlesSpawner3D(settings);
     }
 
@@ -28,6 +30,40 @@ public sealed class SpawnSystem3D
         for (int i = 0; i < localPositions.Length; i++)
         {
             Vector3 worldPos = localToWorld.MultiplyPoint3x4(localPositions[i]);
+
+            spawnData.positions[i] = (float3)worldPos;
+            spawnData.velocities[i] = float3.zero;
+        }
+
+        return spawnData;
+    }
+
+    public SpawnData3D SpawnParticlesInBucket(BucketGenerator bucket)
+    {
+        int n = _settings.particleCount;
+        SpawnData3D spawnData = new SpawnData3D
+        {
+            positions = new float3[n],
+            velocities = new float3[n]
+        };
+
+        float innerRadius = bucket.bottomRadius - bucket.thickness - _settings.radius;
+        float minHeight = bucket.thickness + _settings.radius;
+        float maxHeight = bucket.height + 0.05f;
+
+        Matrix4x4 localToWorld = bucket.transform.localToWorldMatrix;
+
+        for (int i = 0; i < n; i++)
+        {
+            float r = innerRadius * Mathf.Sqrt(UnityEngine.Random.value);
+            float theta = UnityEngine.Random.value * 2f * Mathf.PI;
+            float y = Mathf.Lerp(minHeight, maxHeight, UnityEngine.Random.value);
+
+            float x = r * Mathf.Cos(theta);
+            float z = r * Mathf.Sin(theta);
+
+            Vector3 localPos = new Vector3(x, y, z);
+            Vector3 worldPos = localToWorld.MultiplyPoint3x4(localPos);
 
             spawnData.positions[i] = (float3)worldPos;
             spawnData.velocities[i] = float3.zero;
